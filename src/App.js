fb37cac6d66a54f3035b45f64139596f9d586440
Pedro <jinnpheonix@protonmail.com>
2022-09-15 19:45:42 -0300

    tailwind added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./Components/Navbar";
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import Projects from "./Components/Projects";
+import Skills from "./Components/Skills";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-gray-400 bg-gray-900 body-font">
+      <Navbar/>
+      <About/>
+      <Projects/>
+      <Skills/>
+      <Contact/>
     </div>
   );
 }
