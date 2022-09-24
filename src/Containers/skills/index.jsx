3375f3057db7e2ccb6e227ecec31f592e07d050a
Pedro <jinnpheonix@protonmail.com>
2022-09-24 16:21:07 -0300

    base skills page

--- src/Containers/skills/index.jsx
+++ src/Containers/skills/index.jsx
@@ -1,5 +1,7 @@
 import PageHeaderContent from "../../Components/pageHeaderContent";
 import { BsInfoCircleFill } from 'react-icons/bs';
+import { skillsData } from "./utils";
+import './styles.scss';
 
 const Skills = () => {
     return(
@@ -7,6 +9,22 @@ const Skills = () => {
             <PageHeaderContent 
             headerText="Skills"
             icon={<BsInfoCircleFill size={30}/>}/>
+            <div className="skills__content-wrapper">
+                {
+                    skillsData.map((item,i) =>(
+                        <div key={i} className="skills__content-wrapper__inner-content">
+                            <h3 className="skills__content-wrapper__inner-content__category-text">{item.label}</h3>
+                            <div className="skills__content-wrapper__inner-content__category-text__item-text">
+                                {
+                                    item.data.map((skillItem,j) =>(
+                                        <p key={j}>{skillItem.skillName}</p>
+                                    ))
+                                }
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
         </section>
     )
 }
---
+++ src/Containers/skills/utils.js
@@ -0,0 +1,38 @@
+export const skillsData = [
+    {
+        label: "FRONT END",
+        data:
+        [
+            {
+                skillName: "HTML",
+            },
+            {
+                skillName: "CSS",
+            },
+            {
+                skillName: "Javascript",
+            },
+            {
+                skillName: "React"
+            }
+        ],
+    },
+    {
+        label: "BACK END",
+        data:
+        [
+            {
+                skillName: 'NodeJS',
+            }
+        ]
+    },
+    {
+        label: "DATABASE",
+        data:
+        [
+            {
+                skillName: 'SQL',
+            }
+        ]
+    }
+];
